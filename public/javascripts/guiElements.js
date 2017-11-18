b58a4c6669b9681380d02a3576a0ca38a0a9efc8
Even Grøtte <eogrotte@stud.ntnu.no>
2017-11-18 16:47:29 +0100

    Continued testing, now functional turf-framework and working layers (yay!)

--- public/javascripts/guiElements.js
+++
@@ -1,8 +0,0 @@
-guiElements.initGUI = {
-
-    initMap: function () {
-        var map = L.map('mapid').setView([63.4269, 10.3969],13);
-        var x = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-        x.addTo(map);
-    }
-}
