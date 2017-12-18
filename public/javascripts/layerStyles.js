2fe81ee8b338740ceebb021dac54acb05b7e8db0
Even Grøtte <eogrotte@stud.ntnu.no>
2017-12-18 09:54:08 +0100

    Continued testing, now functional turf-framework and working layers (yay!)

---
+++ public/javascripts/layerStyles.js
@@ -0,0 +1,14 @@
+var waterStyle = {
+    weight: 4,
+    color: "#0720ff",
+    opacity: 1,
+    fillColor: "#7791ff",
+    fillOpacity: 0.5
+};
+var roadStyle = {
+    weight: 4,
+    color: "#000000",
+    opacity: 1,
+    fillColor: "#686868",
+    fillOpacity: 0.5
+};
